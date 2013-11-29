8f694f7a5214c657e447cf765f293ac5ff1e9241
Chris Cooper <chris.cooper@nicta.com.au>
2013-11-29 13:52:19 +1100

    wip

--- app.js
+++ app.js
@@ -1,34 +1,169 @@
 var App = function() {
 };
 
-App.prototype.getCZML = function() {
-	
+var isViewAll = false;
+
+function mix(x, y, a) {
+    return x * (1.0 - a) + y * a;
+}
+
+function mixColors(x, y, a) {
+    return new Cesium.Color(mix(x.red, y.red, a), mix(x.green, y.green, a),
+			mix(x.blue, y.blue, a), mix(x.alpha, y.alpha, a));
+}
+
+App.prototype.getSampleCZML = function(sample){
+	   var avail;
+	    var jsDateStart = sample.start, jsDateStop = sample.stop;
+
+	    // https://github.com/AnalyticalGraphicsInc/cesium/issues/399
+	    avail = jsDateStart.toISOString() + '/' + jsDateStop.toISOString();
+
+	    var a = new Cesium.Color(0.1, 0.1, 1.0, 1.0);
+	    var b = new Cesium.Color(1.0, 0.1, 0.1, 1.0);
+
+	    var normSeverity = (sample.severity - 1.0) / 3.0;
+
+	    var c = mixColors(a, b, normSeverity);
+	    var scale = mix(0.5, 1.0, normSeverity);
+
+	    // only add a label if we're in '24' mode
+	    var label;
+	    var alpha = 1.0;
+
+	    if (isViewAll) {
+	        alpha = 0.5;
+	        // scale *= 0.5;
+	    } else {
+	        label = {
+	            "fillColor": [{
+	                // "interval" : avail,
+	                "rgba": [255, 255, 255, 255]
+	            }],
+	            "font": "10px sans-serif",
+	            "horizontalOrigin": "CENTER",
+	            "outlineColor": {
+	                "rgba": [0, 0, 0, 255]
+	            },
+	            "pixelOffset": {
+	                "cartesian2": [0.0, 40.0 * (0.5 + 0.5 * normSeverity)]
+	            },
+	            "scale": 1.0,
+	            "show": [{
+	                "boolean": true
+	            }],
+	            "style": "FILL",
+	            "text": sample.id.toString(),
+	            "verticalOrigin": "CENTER"
+	        };
+	    }
+
+	    return {
+	        "id": sample.id,
+	        "availability": avail,
+	        "billboard": {
+	            "color": {
+	                "rgba": [Cesium.Color.floatToByte(c.red),
+	                        Cesium.Color.floatToByte(c.green),
+	                        Cesium.Color.floatToByte(c.blue),
+	                        Cesium.Color.floatToByte(alpha)]
+	            },
+	            "eyeOffset": {
+	                "cartesian": [0.0, 0.0, 0.0]
+	            },
+	            "horizontalOrigin": "CENTER",
+	            "image": "images/icon.png",
+	            "pixelOffset": {
+	                "cartesian2": [0.0, 0.0]
+	            },
+	            "scale": scale,
+	            "show": [{
+	                "boolean": true
+	            }],
+	            "verticalOrigin": "BOTTOM"
+	        },
+	        "label": label,
+	        "position": {
+	            "cartographicDegrees": [sample.lon, sample.lat, 10.0]
+	        }
+	    };
 };
 
+App.prototype.getCZML = function(data, viewer) {
+	var samples = data.occurrences;
+//	alert('found: ' + samples.length + ' samples');
+	var scene = viewer.scene;
+	
+	var czml = [];
+	
+	for( var i = 0; i < samples.length; ++i){
+		var s = samples[i];
+		
+        var detail = {};
+        detail.lat = s.decimalLatitude;
+        detail.lon = s.decimalLongitude;
+        detail.id = s.uuid;
+        detail.severity = 1.0;
+        detail.start = new Date(s.eventDate);
+        detail.stop = new Date(detail.start + 365);
+
+		
+        czml.push(this.getSampleCZML(detail));
+	}
+	
+	// console.log(JSON.stringify(czml, undefined, 2));
+	
+	   //console.log('incidentInfo length: ' + incidentInfo);
+
+  // console.log(JSON.stringify(extent));
+
+    // Process the CZML, which populates a collection with DynamicObjects
+    var dataSourceCollection24 = new Cesium.DataSourceCollection();
+    var visualizers24 = new Cesium.DataSourceDisplay(scene, dataSourceCollection24);
+    var czmlDataSource24 = new Cesium.CzmlDataSource();
+    czmlDataSource24.process(czml);
+    dataSourceCollection24.add(czmlDataSource24);
+    var dynamicObjectCollection24 = czmlDataSource24.getDynamicObjectCollection();
+
+    // Figure out the time span of the data
+    var availability = dynamicObjectCollection24.computeAvailability();
+    
+    console.log("****- "+JSON.stringify(availability, undefined, 2));
+	
+    var timeline = viewer.timeline;
+    var clock = viewer.clock;
+    
+    clock.startTime = availability.start.clone();
+    clock.currentTime = availability.start.clone();
+    clock.stopTime = availability.stop.clone();
+    timeline.zoomTo(clock.startTime, clock.stopTime);
+    
+    scene.base_render = scene.render;
+    scene.render = function (date) {
+            visualizers24.update(date);
+        scene.base_render(date);
+    };
+    
+    
+};
 
 App.prototype.init = function() {
 	var options = {
 		timeline : true,
 		animation : true
 	};
-	var cesiumWidget = new Cesium.Viewer('cesiumContainer', options);
-
-
-	var url = 'http://biocache.ala.org.au/ws/occurrences/search?wkt=POLYGON((150.93 -33.78,151.42 -33.80,151.43 -33.51,151.05 -33.54,150.93 -33.78))&q=matched_name:"Strepera graculina"&fq=rank:species&flimit=10&pageSize=1&foffset=0&&facets=names_and_lsid';
+	var viewer = new Cesium.Viewer('cesiumContainer', options);
 	
+	var n = 10;
+	var url = 'http://biocache.ala.org.au/ws/occurrences/search?wkt=POLYGON((150.93 -33.78,151.42 -33.80,151.43 -33.51,151.05 -33.54,150.93 -33.78))&q=matched_name:"Strepera graculina"&fq=rank:species&flimit='
+			+ n + '&pageSize=' + n + '&foffset=0&&facets=names_and_lsid';
+
+	var that = this;
 	function requestSuccess(data) {
-		console.log(JSON.stringify(data));
-		
+		// console.log(JSON.stringify(data, undefined, 2));
+		that.getCZML(data, viewer);
 	}
-	
-	var json = $.getJSON(url, requestSuccess);
 
-	$.ajax({
-		dataType: "jsonp",
-		url: url,
-		//data: data,
-		success: requestSuccess
-		});
-
+	$.getJSON(url, requestSuccess);
 
 };
